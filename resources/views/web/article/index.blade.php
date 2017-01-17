493a17c95975405b62dec396d970487a4f313743
chengshao2014 <cfu_15811072719@163.com>
2017-01-17 16:48:34 +0800

    add some articles

--- resources/views/web/article/index.blade.php
+++ resources/views/web/article/index.blade.php
@@ -45,24 +45,24 @@
                                 <i class="fa fa-refresh"></i>
                             </div>
                         </a>
-                        <div class="blog-time"><?php echo date('m-d',strtotime($article['created_at'])) ?></div>
+                        <div class="blog-time"><?php echo date('m-d',strtotime($article->created_at)) ?></div>
                         <div class="blog-author">
                             <img src = "backend/img/logo/logo_X3.png.png" class="avatar">
-                            <a href="/author/coding">{{$article['author']}}</a></div></div>
+                            <a href="/author/coding">{{$article->author}}</a></div></div>
                 </div>
                 <div class="blog-concise-box">
                     <div class="blog-concise">
                         <div class="blog-title">
-                            <a href="/blog/{{$article['en_id']}}">{{$article['title']}}</a></div>
+                            <a href="/blog/{{$article->en_id}}">{{$article->title}}</a></div>
                         <div class="blog-cover">
-                            <img src="{{$article['thumb']}}">
+                            <img src="{{$article->thumb}}">
                         </div>
                         <div class="ellipsis-text blog-content mui-ellipsis-3">
-                            {{$article['desc']}}
+                            {{$article->desc}}
                         </div>
                         <div class="blog-operate">
                                 <span>
-                                    <a href="/blog/{{$article['en_id']}}">
+                                    <a href="/blog/{{$article->en_id}}">
                                         <i class="fa fa-edit-alt"></i></a></span>
                             <span ><a href="">
                                         <i class="fa fa-trash"></i></a></span></div>
@@ -79,6 +79,8 @@
                     </div>
                 </div>
             </section>@endforeach
+
+
             {{--<section class="blog-box-container">--}}
                 {{--<div class="blog-timeline-box">--}}
                     {{--<div class="blog-timeline">--}}
@@ -115,6 +117,9 @@
     {{--</div>--}}
     <!-- END CONTAINER -->
 
+</div>
+<div class="text-center"">
+{!! $article_list->links() !!}
 </div>
 <!-- END BEGIN PAGE CONTAINER -->
 
