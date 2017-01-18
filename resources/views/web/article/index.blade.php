c22d9e4d73367cdb931d243f5b2b51d181aa0365
chengshao2014 <cfu_15811072719@163.com>
2017-01-18 17:45:48 +0800

    mod permission

--- resources/views/web/article/index.blade.php
+++ resources/views/web/article/index.blade.php
@@ -6,35 +6,10 @@
 
     <!-- BEGIN BREADCRUMBS -->
     <div class=" breadcrumbs margin-bottom-40">
-        {{--<div class="container">--}}
-            {{--<div class="header_sub">--}}
-                {{--<div class="h_menu">--}}
-                    {{--<ul>--}}
-                        {{--<li class="active">--}}
-                            {{--<a href="/">首页</a>--}}
-                        {{--</li>--}}
-                        {{--<li class="active">--}}
-                            {{--<a href="/blog">博客</a>--}}
-                        {{--</li>--}}
-                        {{--<li class="active">--}}
-                            {{--<a href="/blog">视频</a>--}}
-                        {{--</li>--}}
-                        {{--<li class="active">--}}
-                            {{--<a href="/blog">留言板</a>--}}
-                        {{--</li>--}}
-                        {{--<li class="active">--}}
-                            {{--<a href="/blog">关于我们</a>--}}
-                        {{--</li>--}}
-                    {{--</ul>--}}
-                {{--</div>--}}
-
-            {{--</div>--}}
-        {{--</div>--}}
     </div>
     <!-- END BREADCRUMBS -->
 
     <!-- BEGIN CONTAINER -->
-    {{--<div class="container min-hight">--}}
         <div class="main-container">
             @foreach($article_list as $article)
             <section class="blog-box-container">
@@ -45,7 +20,7 @@
                                 <i class="fa fa-refresh"></i>
                             </div>
                         </a>
-                        <div class="blog-time"><?php echo date('m-d',strtotime($article->created_at)) ?></div>
+                        <div class="blog-time"><?php echo date('y-m-d',strtotime($article->created_at)) ?></div>
                         <div class="blog-author">
                             <img src = "backend/img/logo/logo_X3.png.png" class="avatar">
                             <a href="/author/coding">{{$article->author}}</a></div></div>
@@ -79,44 +54,8 @@
                     </div>
                 </div>
             </section>@endforeach
-
-
-            {{--<section class="blog-box-container">--}}
-                {{--<div class="blog-timeline-box">--}}
-                    {{--<div class="blog-timeline">--}}
-                        {{--<a  href="/blog">--}}
-                            {{--<div  class="blog-category">--}}
-                                {{--<i class="fa fa-refresh"></i>--}}
-                            {{--</div>--}}
-                        {{--</a>--}}
-                        {{--<div class="blog-time">12月28日</div>--}}
-                        {{--<div class="blog-author">--}}
-                            {{--<img src = "backend/img/logo/logo_X3.png.png" class="avatar">--}}
-                            {{--<a href="/author/coding">coding</a></div></div>--}}
-                {{--</div>--}}
-            {{--</section>--}}
-            {{--<section class="blog-box-container">--}}
-                {{--<div class="blog-timeline-box">--}}
-                    {{--<div class="blog-timeline">--}}
-                        {{--<a  href="/blog">--}}
-                            {{--<div  class="blog-category">--}}
-                                {{--<i class="fa fa-refresh"></i>--}}
-                            {{--</div>--}}
-                        {{--</a>--}}
-                        {{--<div class="blog-time">12月28日</div>--}}
-                        {{--<div class="blog-author">--}}
-                            {{--<img src = "backend/img/logo/logo_X3.png.png" class="avatar">--}}
-                            {{--<a href="/author/coding">coding</a>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                    {{----}}
-                {{--</div>--}}
-            {{--</section>--}}
-
         </div>
-    {{--</div>--}}
     <!-- END CONTAINER -->
-
 </div>
 <div class="text-center"">
 {!! $article_list->links() !!}
